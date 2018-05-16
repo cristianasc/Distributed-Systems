e6bc22f8696b8a868a94c9dfd28eb88cbd61a46d
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2018-05-16 12:52:12 +0100

    StableStart done

---
+++ Project3/src/monitors/Stable/StableStart.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monitors.Stable;
+
+import interfaces.*;
+import java.rmi.*;
+import static java.rmi.registry.LocateRegistry.getRegistry;
+import java.rmi.registry.*;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author cristianacarvalho
+ */
+public class StableStart {
+    
+    private static int SERVER_PORT;
+    private static String rmiServerHostname;
+    private static int rmiServerPort;
+    private static String nameEntryBase = "RegisterHandler";
+    private static String nameEntryObject = "Stable";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws RemoteException {
+        System.out.print("Starting Stable");
+        SERVER_PORT = Integer.parseInt(args[0]);
+        rmiServerHostname = args[1];
+        rmiServerPort = Integer.parseInt(args[2]);
+        
+        Registry registry = getRegistry(rmiServerHostname, rmiServerPort);
+        Register register = getRegister(registry);
+        IGeneralRepository gr = getGenerealRepository(registry);
+        
+        /* create and install the security manager */
+        if (System.getSecurityManager () == null){
+            System.setSecurityManager (new SecurityManager ());
+            System.out.println("Security manager was installed!");
+        }
+        
+        Stable st = new Stable((IGeneralRepository) gr);
+        IStable iSt = null;
+        
+        try{ 
+            iSt = (IStable) UnicastRemoteObject.exportObject((Remote) st, SERVER_PORT);
+        }catch (RemoteException e){ 
+            System.out.println("ComputeEngine stub generation exception: " + e.getMessage ());
+            e.printStackTrace ();
+            System.exit(1);
+        }
+        
+        try{ 
+            register.bind (nameEntryObject, (Remote) iSt);
+        }catch (RemoteException e){ 
+            System.out.println("ComputeEngine registration exception: " + e.getMessage ());
+            e.printStackTrace ();
+            System.exit(1);
+        }catch (AlreadyBoundException e){ 
+            System.out.println("ComputeEngine already bound exception: " + e.getMessage ());
+            e.printStackTrace ();
+            System.exit(1);
+     }
+     System.out.println("ComputeEngine object was registered!");
+        
+        
+    }
+    
+    private static Register getRegister(Registry registry){
+        Register register = null;
+        try{ 
+            register = (Register) registry.lookup(nameEntryObject);
+        }catch (RemoteException e){ 
+            System.out.println("RegisterRemoteObject lookup exception: " + e.getMessage ());
+            e.printStackTrace ();
+            System.exit(1);
+        }catch (NotBoundException e){ 
+            System.out.println("RegisterRemoteObject not bound exception: " + e.getMessage ());
+            e.printStackTrace ();
+            System.exit(1);
+        }
+        return register;
+    }
+    
+    
+    private static IGeneralRepository getGenerealRepository(Registry registry){
+        IGeneralRepository gr = null;
+        
+        try{ 
+            gr = (IGeneralRepository) registry.lookup("GeneralRepository");
+        }catch (RemoteException e){ 
+            System.exit(1);
+        }catch (NotBoundException e){
+            System.exit(1);
+        }
+        
+        return gr;
+    }
+}
