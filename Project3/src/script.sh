e41930f816f167caa25bd4944015b482ba90633f
Miguel Maia <maiamiguel@ua.pt>
2018-06-10 16:14:58 +0100

    2

--- Project3/src/script.sh
+++ Project3/src/script.sh
@@ -8,7 +8,7 @@ ControlCenterHostName=l040101-ws03.ua.pt
 BettingCenterHostName=l040101-ws04.ua.pt
 RaceTrackHostName=l040101-ws05.ua.pt
 StableHostName=l040101-ws06.ua.pt
-PaddockHostName=l040101-ws07.ua.pt
+PaddockHostName=l040101-ws06.ua.pt
 BrokerHostName=l040101-ws08.ua.pt
 HorsesHostName=l040101-ws09.ua.pt
 SpectatorHostName=l040101-ws10.ua.pt
@@ -54,7 +54,7 @@ sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHost
 
 echo "Setting RMI repository.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName " rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 22460;" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName " rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 5000;" &
 sleep 2
 sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName " java 
 -Djava.rmi.server.codebase="$(pwd)"\
@@ -67,61 +67,61 @@ echo " "
 
 echo "Setting General Repository.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$GeneralRepositoryHostName " java monitors/GeneralRepository/GeneralRepositoryStart 22170 l040101-ws02.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$GeneralRepositoryHostName " java monitors/GeneralRepository/GeneralRepositoryStart 22170 l040101-ws02.ua.pt 5000" &
 sleep 5
 echo " "
 
 
 echo "Setting Control Center.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$ControlCenterHostName " java monitors/ControlCentre/ControlCentreStart 22174 l040101-ws03.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$ControlCenterHostName " java monitors/ControlCentre/ControlCentreStart 22174 l040101-ws03.ua.pt 5000" &
 sleep 5
 echo " "
 
 
 echo "Setting Betting Center.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BettingCenterHostName " java monitors/BettingCentre/BettingCentreStart 22173 l040101-ws04.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BettingCenterHostName " java monitors/BettingCentre/BettingCentreStart 22173 l040101-ws04.ua.pt 5000" &
 sleep 5
 echo " "
 
 
 echo "Setting RaceTrack.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$RaceTrackHostName " java monitors/RacingTrack/RacingTrackStart 22175 l040101-ws05.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$RaceTrackHostName " java monitors/RacingTrack/RacingTrackStart 22175 l040101-ws05.ua.pt 5000" &
 sleep 5
 echo " "
 
 
 echo "Setting Stable.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$StableHostName "java monitors/Stable/StableStart 22171 l040101-ws06.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$StableHostName "java monitors/Stable/StableStart 22171 l040101-ws06.ua.pt 5000" &
 sleep 5
 echo " "
 
 
 echo "Setting Paddock.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$PaddockHostName " java monitors/Paddock/PaddockStart 22172 l040101-ws07.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$PaddockHostName " java monitors/Paddock/PaddockStart 22172 l040101-ws06.ua.pt 5000" &
 sleep 5
 echo " "
 
 
 echo "Setting Broker.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BrokerHostName " java monitors/main/BrokerStart 22179 l040101-ws08.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BrokerHostName " java monitors/main/BrokerStart 22179 l040101-ws08.ua.pt 5000" &
 sleep 5
 echo " "
 
 echo "Setting Horses.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$HorsesHostName " java monitors/main/HorseStart 22178 l040101-ws09.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$HorsesHostName " java monitors/main/HorseStart 22178 l040101-ws09.ua.pt 5000" &
 sleep 5
 echo " "
 
 echo "Setting Spectator.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHostName " java monitors/main/SpectatorStart 22177 l040101-ws10.ua.pt 22460" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHostName " java monitors/main/SpectatorStart 22177 l040101-ws10.ua.pt 5000" &
 sleep 5
 echo " "
 
