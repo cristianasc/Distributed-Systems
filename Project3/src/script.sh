f8f1e158adde126fb4a8d0296b866312a9d96c84
Miguel Maia <maiamiguel@ua.pt>
2018-06-10 15:44:17 +0100

    changes in script

--- Project3/src/script.sh
+++ Project3/src/script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 username=sd0107
 password=grupomaisfixedesempre96
-url=http://192.168.8.171/sd0407/classes/
+url=http://192.168.8.171/sd0107/classes/
 registryHostName=l040101-ws01.ua.pt
 GeneralRepositoryHostName=l040101-ws02.ua.pt
 ControlCenterHostName=l040101-ws03.ua.pt
@@ -21,42 +21,42 @@ tar -czf code.tar.gz registry/ monitors/ main/ interfaces/ states/
 sleep 5
 
 echo "Unziping code in machines"
-sshpass -p $password scp code.tar.gz $username@$registryHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName "cd ~/Public/ ; tar -xmzf code.tar.gz" &
+sshpass -p $password scp code.tar.gz $username@$registryHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName " tar -xmzf code.tar.gz" &
 
-sshpass -p $password scp code.tar.gz $username@$GeneralRepositoryHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$GeneralRepositoryHostName "cd ~/Public/ ; tar -xmzf code.tar.gz;" &
+sshpass -p $password scp code.tar.gz $username@$GeneralRepositoryHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$GeneralRepositoryHostName " tar -xmzf code.tar.gz;" &
 
-sshpass -p $password scp code.tar.gz $username@$PaddockHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$PaddockHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$PaddockHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$PaddockHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$StableHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$StableHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$StableHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$StableHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$BettingCenterHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BettingCenterHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$BettingCenterHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BettingCenterHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$RaceTrackHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$RaceTrackHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$RaceTrackHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$RaceTrackHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$ControlCenterHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$ControlCenterHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$ControlCenterHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$ControlCenterHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$BrokerHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BrokerHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$BrokerHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BrokerHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$HorsesHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$HorsesHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$HorsesHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$HorsesHostName " tar -xmzf code.tar.gz; " &
 
-sshpass -p $password scp code.tar.gz $username@$SpectatorHostName:~/Public/
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHostName "cd ~/Public/ ; tar -xmzf code.tar.gz; " &
+sshpass -p $password scp code.tar.gz $username@$SpectatorHostName:
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHostName " tar -xmzf code.tar.gz; " &
 
 
 echo "Setting RMI repository.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName "cd ~/Public/ ; rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 5000;" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName " rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false 22460;" &
 sleep 2
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName "cd ~/Public/ ; java 
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$registryHostName " java 
 -Djava.rmi.server.codebase="$(pwd)"\
      -Djava.rmi.server.useCodebaseOnly=false\
      -Djava.security.policy=java.policy\
@@ -67,61 +67,61 @@ echo " "
 
 echo "Setting General Repository.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$GeneralRepositoryHostName "cd ~/Public/ ; java monitors/GeneralRepository/GeneralRepositoryStart 22170 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$GeneralRepositoryHostName " java monitors/GeneralRepository/GeneralRepositoryStart 22170 l040101-ws02.ua.pt 22460" &
 sleep 5
 echo " "
 
 
 echo "Setting Control Center.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$ControlCenterHostName "cd ~/Public/ ; java monitors/ControlCentre/ControlCentreStart 22174 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$ControlCenterHostName " java monitors/ControlCentre/ControlCentreStart 22174 l040101-ws03.ua.pt 22460" &
 sleep 5
 echo " "
 
 
 echo "Setting Betting Center.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BettingCenterHostName "cd ~/Public/ ; java monitors/BettingCentre/BettingCentreStart 22173 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BettingCenterHostName " java monitors/BettingCentre/BettingCentreStart 22173 l040101-ws04.ua.pt 22460" &
 sleep 5
 echo " "
 
 
 echo "Setting RaceTrack.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$RaceTrackHostName "cd ~/Public/ ; java monitors/RacingTrack/RacingTrackStart 22175 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$RaceTrackHostName " java monitors/RacingTrack/RacingTrackStart 22175 l040101-ws05.ua.pt 22460" &
 sleep 5
 echo " "
 
 
 echo "Setting Stable.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$StableHostName "cd ~/Public/ ; java monitors/Stable/StableStart 22171 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$StableHostName "java monitors/Stable/StableStart 22171 l040101-ws06.ua.pt 22460" &
 sleep 5
 echo " "
 
 
 echo "Setting Paddock.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$PaddockHostName "cd ~/Public/ ; java monitors/Paddock/PaddockStart 22172 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$PaddockHostName " java monitors/Paddock/PaddockStart 22172 l040101-ws07.ua.pt 22460" &
 sleep 5
 echo " "
 
 
 echo "Setting Broker.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BrokerHostName "cd ~/Public/ ; java monitors/main/BrokerStart 22179 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$BrokerHostName " java monitors/main/BrokerStart 22179 l040101-ws08.ua.pt 22460" &
 sleep 5
 echo " "
 
 echo "Setting Horses.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$HorsesHostName "cd ~/Public/ ; java monitors/main/HorseStart 22178 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$HorsesHostName " java monitors/main/HorseStart 22178 l040101-ws09.ua.pt 22460" &
 sleep 5
 echo " "
 
 echo "Setting Spectator.... "
 echo " "
-sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHostName "cd ~/Public/ ; java monitors/main/SpectatorStart 22177 localhost 5000" &
+sshpass -p $password ssh -o StrictHostKeyChecking=no -f $username@$SpectatorHostName " java monitors/main/SpectatorStart 22177 l040101-ws10.ua.pt 22460" &
 sleep 5
 echo " "
 
